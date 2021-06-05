9c5b30da8da5ca333c70d9cb413c45520833b14c
Jeshica216 <Jessicaangelinagatur21@gmail.com>
2021-06-05 13:50:08 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Dokter246Controller;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dokter0246', 'App\Http\Controllers\DokterController@index');
+// Route::resource('/dokter', [DokterController::class]);
